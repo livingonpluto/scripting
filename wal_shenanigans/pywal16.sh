15652ad8562e225bbca2d0e8f32e5b6d0f0ca0b5
livingonpluto <verkauftmeinedaten@gmail.com>
2026-01-15 18:52:10 +0100

    debian shortcut

--- wal_shenanigans/pywal16.sh
+++ wal_shenanigans/pywal16.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 # pywalfox update
+
+LOGFILE=~/Stuff/scripting/wal_shenanigans/log.log
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOGFILE"
+}
+
+log "Now followup scripts"
